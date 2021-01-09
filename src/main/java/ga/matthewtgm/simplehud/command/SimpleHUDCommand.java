292821f4a9fec15dd1bdbeb7f42347855db5feaf
leafs <josh@vilifytech.com>
2021-01-09 17:13:44 -0500

    fix command

--- src/main/java/ga/matthewtgm/simplehud/command/SimpleHUDCommand.java
+++ src/main/java/ga/matthewtgm/simplehud/command/SimpleHUDCommand.java
@@ -1,9 +1,15 @@
 package ga.matthewtgm.simplehud.command;
 
+import ga.matthewtgm.simplehud.SimpleHUD;
 import ga.matthewtgm.simplehud.utils.ChatUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class SimpleHUDCommand extends CommandBase {
 
@@ -25,7 +31,24 @@ public class SimpleHUDCommand extends CommandBase {
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
         if(!(sender instanceof EntityPlayer)) return;
-        new ChatUtils().sendModMessage("Please use the keybind, N");
+
+        /*  fixed code in-case you don't like the GUI opening on command
+
+            String bind = GameSettings.getKeyDisplayString(SimpleHUD.getInstance().openGuiKeyBinding.getKeyCode());
+            // this also can be static if it's a utility method (not all statics are static abuse)
+            new ChatUtils().sendModMessage(String.format("Please use the keybind, %s", bind));
+
+        */
+
+        // cause a tick delay on opening
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        // display the GUI and unregister this tick event
+        Minecraft.getMinecraft().displayGuiScreen(SimpleHUD.getInstance().configGui);
+        MinecraftForge.EVENT_BUS.unregister(this);
     }
 
 }
